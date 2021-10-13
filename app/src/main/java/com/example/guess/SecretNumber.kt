b405e20385eda9a4cfeb28a704bb56578072fcbc
Bill <abcde5676151199@gmail.com>
2021-10-13 16:26:20 +0800

    玩家輸入畫面

--- app/src/main/java/com/example/guess/SecretNumber.kt
+++ app/src/main/java/com/example/guess/SecretNumber.kt
@@ -3,21 +3,20 @@ package com.example.guess
 import java.util.*
 
 class SecretNumber{
-    var secret = Random().nextInt(10)+1
+    //var secret = Random().nextInt(10)+1
     var count = 0
-    fun validate(number : Int) : Int{
+    fun validate(number : Int, secret:Int) : Int{
         count++
         return number-secret
     }
-    fun reset(){
+    /*fun reset(){
         secret = Random().nextInt(10)+1
         count = 0
-    }
+    }*/
 }
 
 fun main(){
     val secretNumber = SecretNumber()
-    println(secretNumber.secret)
-    println("${secretNumber.validate(2)}, count : ${secretNumber.count}")
-    println("${secretNumber.validate(2)}, count : ${secretNumber.count}")
+    //println(secretNumber.secret)
+    println("${secretNumber.validate(2, 10)}, count : ${secretNumber.count}")
 }
